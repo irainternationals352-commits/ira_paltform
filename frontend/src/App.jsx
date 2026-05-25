ab4616372248c510ce3abda81719feeca06dce83
SatishMaurya09 <satish.mayura@solytics-partners.com>
2026-05-25 23:28:29 +0530

    fix: added global scroll reset on router navigations to scroll to top automatically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -24,10 +24,25 @@ import CountriesManager from './pages/admin/CountriesManager';
 import ServicesManager from './pages/admin/ServicesManager';
 import ProgramsManager from './pages/admin/ProgramsManager';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant'
+    });
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           {/* Admin Routes */}
           <Route path="/admin-login" element={<AdminLogin />} />
